1d757db54a98a563b69b56bff49e76de70c1302f
211110476_jimmy <211110476@students.mikroskil.ac.id>
2023-06-11 18:47:10 +0700

    mulai membuat flutter

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       // home: MainPage(),
-      home: welcomeScreen(),
+      // home: welcomeScreen(),
     );
   }
 }
